6935e2c3eda54162eaa0e17c4b40cf77a1358452
NewUser024 <61599374+NewUser024@users.noreply.github.com>
2021-11-19 17:46:42 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -49,45 +49,45 @@ import PySimpleGUI as sg
 sg.theme('Green')
 
 col_test = [[ sg.Text(data_test_questions ['1.'])],
-        [sg.Radio(data_test_answers ['1)'], "RADIO1", key = '-INPUT-', default=False), sg.Radio('700',"RADIO1", default = False), sg.Radio('725', "RADIO1", default = False), sg.Radio('5', "RADIO1", default = False)],
+        [sg.Radio(data_test_answers ['1)'], "RADIO1", key = 'INPUT1', default=False), sg.Radio('700',"RADIO1", default = False), sg.Radio('725', "RADIO1", default = False), sg.Radio('5', "RADIO1", default = False)],
         [sg.Text(data_test_questions ['2.'])],
-        [sg.Radio('(3;-5; 2)', "RADIO2", default = False), sg.Radio(data_test_answers['2)'], "RADIO2", key = '-INPUT-', default=False), sg.Radio('(0;7;2)', "RADIO2", default = False), sg.Radio('(-2;-5;7)', "RADIO2", default = False)],
+        [sg.Radio('(3;-5; 2)', "RADIO2", default = False), sg.Radio(data_test_answers['2)'], "RADIO2", key = 'INPUT2', default=False), sg.Radio('(0;7;2)', "RADIO2", default = False), sg.Radio('(-2;-5;7)', "RADIO2", default = False)],
         [sg.Text(data_test_questions ['3.'])],
-        [sg.Radio('0', "RADIO3", default = False), sg.Radio('765', "RADIO3", default = False), sg.Radio('-7', "RADIO3", default = False), sg.Radio(data_test_answers ['3)'], "RADIO3", key = '-INPUT-', default=False)],
+        [sg.Radio('0', "RADIO3", default = False), sg.Radio('765', "RADIO3", default = False), sg.Radio('-7', "RADIO3", default = False), sg.Radio(data_test_answers ['3)'], "RADIO3", key = 'INPUT3', default=False)],
         [sg.Text(data_test_questions ['4.'])],
-        [sg.Radio('45', "RADIO4", default = False), sg.Radio('0,4', "RADIO4", default = False), sg.Radio(data_test_answers['4)'], "RADIO4", key = '-INPUT-', default=False), sg.Radio('0', "RADIO4", default = False)],
+        [sg.Radio('45', "RADIO4", default = False), sg.Radio('0,4', "RADIO4", default = False), sg.Radio(data_test_answers['4)'], "RADIO4", key = 'INPUT4', default=False), sg.Radio('0', "RADIO4", default = False)],
         [sg.Text(data_test_questions ['5.'])],
-        [sg.Radio('50см', "RADIO5", default = False), sg.Radio('0см', "RADIO5", default = False), sg.Radio(data_test_answers ['5)'], "RADIO5", key = '-INPUT-', default=False), sg.Radio('66см', "RADIO5", default = False)],
+        [sg.Radio('50см', "RADIO5", default = False), sg.Radio('0см', "RADIO5", default = False), sg.Radio(data_test_answers ['5)'], "RADIO5", key = 'INPUT5', default=False), sg.Radio('66см', "RADIO5", default = False)],
         [sg.Text(data_test_questions ['6.'])],
-        [sg.Radio('100', "RADIO6", default = False), sg.Radio('120', "RADIO6", default = False), sg.Radio(data_test_answers['6)'], "RADIO6", key = '-INPUT-', default=False), sg.Radio('-31', "RADIO6", default = False)],
+        [sg.Radio('100', "RADIO6", default = False), sg.Radio('120', "RADIO6", default = False), sg.Radio(data_test_answers['6)'], "RADIO6", key = 'INPUT6', default=False), sg.Radio('-31', "RADIO6", default = False)],
         [sg.Text(data_test_questions ['7.'])],
-        [sg.Radio('(8;-2;0)', "RADIO7", default = False), sg.Radio(data_test_answers ['7)'], "RADIO7", key = '-INPUT-', default=False), sg.Radio('-4;2;10', "RADIO7", default = False), sg.Radio('(0;0;0)', "RADIO7", default = False)],
+        [sg.Radio('(8;-2;0)', "RADIO7", default = False), sg.Radio(data_test_answers ['7)'], "RADIO7", key = 'INPUT7', default=False), sg.Radio('-4;2;10', "RADIO7", default = False), sg.Radio('(0;0;0)', "RADIO7", default = False)],
         [sg.Text(data_test_questions ['8.'])],
-        [sg.Radio('230', "RADIO8", default = False), sg.Radio('0', "RADIO8", default = False), sg.Radio('400', "RADIO8", default = False), sg.Radio(data_test_answers['8)'], "RADIO8", key = '-INPUT-', default=False)],
+        [sg.Radio('230', "RADIO8", default = False), sg.Radio('0', "RADIO8", default = False), sg.Radio('400', "RADIO8", default = False), sg.Radio(data_test_answers['8)'], "RADIO8", key = 'INPUT8', default=False)],
         [sg.Text(data_test_questions ['9.'])],
-        [sg.Radio(data_test_answers ['9)'], "RADIO9", key = '-INPUT-', default=False), sg.Radio('(0;2)', "RADIO9", default = False), sg.Radio('-99;-4', "RADIO9", default = False), sg.Radio('2;∞', "RADIO9", default = False)],
+        [sg.Radio(data_test_answers ['9)'], "RADIO9", key = 'INPUT9', default=False), sg.Radio('(0;2)', "RADIO9", default = False), sg.Radio('-99;-4', "RADIO9", default = False), sg.Radio('2;∞', "RADIO9", default = False)],
         [sg.Text(data_test_questions ['10.'])],
-        [sg.Radio(data_test_answers['10)'], "RADIO10", key = '-INPUT-', default=False), sg.Radio('240', "RADIO10", default = False), sg.Radio('7', "RADIO10", default = False), sg.Radio('150', "RADIO10", default = False)],
+        [sg.Radio(data_test_answers['10)'], "RADIO10", key = 'INPUT10', default=False), sg.Radio('240', "RADIO10", default = False), sg.Radio('7', "RADIO10", default = False), sg.Radio('150', "RADIO10", default = False)],
         [sg.Text(data_test_questions ['11.'])],
-        [sg.Radio('3a-5', "RADIO11", default = False), sg.Radio('a', "RADIO11", default = False), sg.Radio('-a', "RADIO11", default = False), sg.Radio(data_test_answers ['11)'], "RADIO11", key = '-INPUT-', default=False)],
+        [sg.Radio('3a-5', "RADIO11", default = False), sg.Radio('a', "RADIO11", default = False), sg.Radio('-a', "RADIO11", default = False), sg.Radio(data_test_answers ['11)'], "RADIO11", key = 'INPUT11', default=False)],
         [sg.Text(data_test_questions ['12.'])],
-        [sg.Radio('5', "RADIO12", default = False), sg.Radio(data_test_answers['12)'], "RADIO12", key = '-INPUT-', default=False), sg.Radio('0', "RADIO12", default = False), sg.Radio('144', "RADIO12", default = False)],
+        [sg.Radio('5', "RADIO12", default = False), sg.Radio(data_test_answers['12)'], "RADIO12", key = 'INPUT12', default=False), sg.Radio('0', "RADIO12", default = False), sg.Radio('144', "RADIO12", default = False)],
         [sg.Text(data_test_questions ['13.'])],
-        [sg.Radio('5-3n', "RADIO13", default = False), sg.Radio('2n', "RADIO13", default = False), sg.Radio(data_test_answers ['13)'], "RADIO13", key = '-INPUT-', default=False), sg.Radio('-n', "RADIO13", default = False)],
+        [sg.Radio('5-3n', "RADIO13", default = False), sg.Radio('2n', "RADIO13", default = False), sg.Radio(data_test_answers ['13)'], "RADIO13", key = 'INPUT13', default=False), sg.Radio('-n', "RADIO13", default = False)],
         [sg.Text(data_test_questions ['14.'])],
-        [sg.Radio(data_test_answers['14)'], "RADIO14", key = '-INPUT-', default=False), sg.Radio('(4;10)', "RADIO14", default = False), sg.Radio('7;21', "RADIO14", default = False), sg.Radio('-4;-2', "RADIO14", default = False)],
+        [sg.Radio(data_test_answers['14)'], "RADIO14", key = 'INPUT14', default=False), sg.Radio('(4;10)', "RADIO14", default = False), sg.Radio('7;21', "RADIO14", default = False), sg.Radio('-4;-2', "RADIO14", default = False)],
         [sg.Text(data_test_questions ['15.'])],
-        [sg.Radio('55см', "RADIO15", default = False), sg.Radio(data_test_answers ['15)'], "RADIO15", key = '-INPUT-', default=False), sg.Radio('Інше', "RADIO15", default = False), sg.Radio('39см', "RADIO15", default = False)],
+        [sg.Radio('55см', "RADIO15", default = False), sg.Radio(data_test_answers ['15)'], "RADIO15", key = 'INPUT15', default=False), sg.Radio('Інше', "RADIO15", default = False), sg.Radio('39см', "RADIO15", default = False)],
         [sg.Text(data_test_questions ['16.'])],
-        [sg.Radio('865', "RADIO16", default = False), sg.Radio(data_test_answers['16)'], "RADIO16", key = '-INPUT-', default=False), sg.Radio('144', "RADIO16", default = False), sg.Radio('920', "RADIO16", default = False)],
+        [sg.Radio('865', "RADIO16", default = False), sg.Radio(data_test_answers['16)'], "RADIO16", key = 'INPUT16', default=False), sg.Radio('144', "RADIO16", default = False), sg.Radio('920', "RADIO16", default = False)],
         [sg.Text(data_test_questions ['17.'])],
-        [sg.Radio('[7; 55]', "RADIO17", default = False), sg.Radio('(-11; 23)', "RADIO17", default = False), sg.Radio(data_test_answers ['17)'], "RADIO17", key = '-INPUT-', default=False), sg.Radio('(-31; -13)', "RADIO17", default = False)],
+        [sg.Radio('[7; 55]', "RADIO17", default = False), sg.Radio('(-11; 23)', "RADIO17", default = False), sg.Radio(data_test_answers ['17)'], "RADIO17", key = 'INPUT17', default=False), sg.Radio('(-31; -13)', "RADIO17", default = False)],
         [sg.Text(data_test_questions ['18.'])],
-        [sg.Radio(data_test_answers['18)'], "RADIO18", key = '-INPUT-', default=False), sg.Radio('Чотири', "RADIO18", default = False), sg.Radio('Безліч', "RADIO18", default = False), sg.Radio('Дві', "RADIO18", default = False)],
+        [sg.Radio(data_test_answers['18)'], "RADIO18", key = 'INPUT18', default=False), sg.Radio('Чотири', "RADIO18", default = False), sg.Radio('Безліч', "RADIO18", default = False), sg.Radio('Дві', "RADIO18", default = False)],
         [sg.Text(data_test_questions ['19.'])],
-        [sg.Radio('65', "RADIO19", default = False), sg.Radio(data_test_answers ['19)'], "RADIO19", key = '-INPUT-', default=False), sg.Radio('50', "RADIO19", default = False), sg.Radio('-75', "RADIO19", default = False)],
+        [sg.Radio('65', "RADIO19", default = False), sg.Radio(data_test_answers ['19)'], "RADIO19", key = 'INPUT19', default=False), sg.Radio('50', "RADIO19", default = False), sg.Radio('-75', "RADIO19", default = False)],
         [sg.Text(data_test_questions ['20.'])],
-        [sg.Radio('75', "RADIO20", default = False), sg.Radio('35', "RADIO20", default = False), sg.Radio('20', "RADIO20", default = False), sg.Radio(data_test_answers['20)'], "RADIO20", key = '-INPUT-', default=False)],
+        [sg.Radio('75', "RADIO20", default = False), sg.Radio('35', "RADIO20", default = False), sg.Radio('20', "RADIO20", default = False), sg.Radio(data_test_answers['20)'], "RADIO20", key = 'INPUT20', default=False)],
         [sg.Button('Submit', size = (20, 3))]]
 s = 0
 
@@ -100,8 +100,9 @@ while True:
         if event == sg.WIN_CLOSED:
                 break
         if event == 'Submit':
-                if values['-INPUT-']== True:
-                        s= +1
-        print(values['-INPUT-'])
-                
+                answers_boolean = [0]
+                for i in range(1, 20):
+                        answers_boolean.append (values['INPUT' + str(i)])
+                s = sum(answers_boolean)
+                print (s)
 window_quiz.close()
