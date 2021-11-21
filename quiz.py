14b3c98d4bfb644b6d796173c7ed077ed4a28c80
NewUser024 <61599374+NewUser024@users.noreply.github.com>
2021-11-21 20:23:51 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -43,12 +43,57 @@ data_test_answers = {
         '20)' : '65'
 
 }
+data_fizik_questions = {
+        '1' : 'Що називається лінзою?',
+        '2' : 'Які вам відомі види лінз?',
+        '3' : 'Що таке фокус лінзи?',
+        '4' : 'Що називається фокусною відстанню лінзи?',
+        '5' : 'Що таке оптична сила лінзи?',
+        '6' : 'Перехід з рідкого стану в газоподібний називається',
+        '7' : 'Яку кількість теплоти потрібно надати 1 кг спирту за температури 38С, щоб довести його до кипіння?',
+        '8' : 'Скільки теплоти потрібно витратити полярникам, щоб перетворити на воду 10 кг льоду, узятого за температури -40С?',
+        '9' : 'Чи можна в Оловяній посудині розплавити цинкову пластинку',
+        '10' : 'Центр тяжіння трикутника знаходиться у точці перетину',
+        '11' : 'Найменшу відстань від осі обертання до лінії дії сили називають',
+        '12' : 'Обчислити значення сили, якщо її плече дорівнює 15 см. Момент цієї сили становить 3 Н⋅м',
+        '13' : 'Яким буде зображення в збиральній лінзі, якщо предмет розташувати у фокусі лінзи?',
+        '14' : 'Оптична сила лінзи дорівнює - 2 дптр. Знайдіть фокусну відстань лінзи і укажіть її вид.',
+        '15' : 'Фокусна відстань розсіювальної лінзи дорівнює 40 см. Знайдіть оптичну силу лінзи?',
+        '16' : 'Точка, у якій перетинається паралельний пучок променів після заломлення в лінзі, називають',
+        '17' : 'Яка фізична величина чисельно дорівнює відношенню маси до обєму',
+        '18' : 'Який із наведених виразів використовується для обчислення маси тіла',
+        '19' : 'Із сталі, латуні і дуба виготовили три кулі однакового обєму. Яка з цих куль має найбільшу масу',
+        '20' : 'Прямокутна металева пластинка розмірами 5см⋅3 см⋅0,5 см має масу 85 г. З якого металу вона може бути виготовлена'
+}
+data_fizik_answers = {
+        '1' : 'прозоре (зазвичай скляне) тіло, обмежене двома сферичними поверхнями',
+        '2' : 'опуклі лінзи, увігнуті лінзи',
+        '3' : 'Точка, в якій перетинаються після заломлення в лінзі промені, називають фокусом лінзи',
+        '4' : 'Відстань від оптичного центру лінзи до її головного фокусу називається фокусною відстанню лінзи',
+        '5' : 'Фізична величина, обернена до фокусної відстані лінзи',
+        '6' : 'Випаровування',
+        '7' : '100 кДж',
+        '8' : '4140 кДж',
+        '9' : 'ні',
+        '10' : 'Його медіан',
+        '11' : 'Плечем сили',
+        '12' : '20 Н',
+        '13' : 'зображення не буде',
+        '14' : '0,5 м, розсіювальна',
+        '15' : '- 2,5 дптр',
+        '16' : 'дійсним головним фокусом лінзи',
+        '17' : 'густина',
+        '18' : 'ρV',
+        '19' : 'латунна',
+        '20' : 'свинцю'
+}
 
+from os import scandir
 import PySimpleGUI as sg
 
 sg.theme('Green')
 
-col_test = [[ sg.Text(data_test_questions ['1.'])],
+col_quiz_math = [[ sg.Text(data_test_questions ['1.'])],
         [sg.Radio(data_test_answers ['1)'], "RADIO1", key = 'INPUT1', default=False), sg.Radio('700',"RADIO1", default = False), sg.Radio('725', "RADIO1", default = False), sg.Radio('5', "RADIO1", default = False)],
         [sg.Text(data_test_questions ['2.'])],
         [sg.Radio('(3;-5; 2)', "RADIO2", default = False), sg.Radio(data_test_answers['2)'], "RADIO2", key = 'INPUT2', default=False), sg.Radio('(0;7;2)', "RADIO2", default = False), sg.Radio('(-2;-5;7)', "RADIO2", default = False)],
@@ -89,14 +134,61 @@ col_test = [[ sg.Text(data_test_questions ['1.'])],
         [sg.Text(data_test_questions ['20.'])],
         [sg.Radio('75', "RADIO20", default = False), sg.Radio('35', "RADIO20", default = False), sg.Radio('20', "RADIO20", default = False), sg.Radio(data_test_answers['20)'], "RADIO20", key = 'INPUT20', default=False)],
         [sg.Button('Submit', size = (20, 3))]]
+
+col_quiz_fiz = [[ sg.Text(data_fizik_questions ['1'])],
+        [sg.Radio(data_fizik_answers ['1'], "RADIO1", key = 'INPUT1', default=False), sg.Radio('700',"RADIO1", default = False), sg.Radio('725', "RADIO1", default = False), sg.Radio('5', "RADIO1", default = False)],
+        [sg.Text(data_fizik_questions ['2'])],
+        [sg.Radio('(3;-5; 2)', "RADIO2", default = False), sg.Radio(data_fizik_answers['2'], "RADIO2", key = 'INPUT2', default=False), sg.Radio('(0;7;2)', "RADIO2", default = False), sg.Radio('(-2;-5;7)', "RADIO2", default = False)],
+        [sg.Text(data_fizik_questions ['3'])],
+        [sg.Radio('0', "RADIO3", default = False), sg.Radio('765', "RADIO3", default = False), sg.Radio('-7', "RADIO3", default = False), sg.Radio(data_fizik_answers ['3'], "RADIO3", key = 'INPUT3', default=False)],
+        [sg.Text(data_fizik_questions ['4'])],
+        [sg.Radio('45', "RADIO4", default = False), sg.Radio('0,4', "RADIO4", default = False), sg.Radio(data_fizik_answers['4'], "RADIO4", key = 'INPUT4', default=False), sg.Radio('0', "RADIO4", default = False)],
+        [sg.Text(data_fizik_questions ['5'])],
+        [sg.Radio('50см', "RADIO5", default = False), sg.Radio('0см', "RADIO5", default = False), sg.Radio(data_fizik_answers ['5'], "RADIO5", key = 'INPUT5', default=False), sg.Radio('66см', "RADIO5", default = False)],
+        [sg.Text(data_fizik_questions ['6'])],
+        [sg.Radio('100', "RADIO6", default = False), sg.Radio('120', "RADIO6", default = False), sg.Radio(data_fizik_answers['6'], "RADIO6", key = 'INPUT6', default=False), sg.Radio('-31', "RADIO6", default = False)],
+        [sg.Text(data_fizik_questions ['7'])],
+        [sg.Radio('(8;-2;0)', "RADIO7", default = False), sg.Radio(data_fizik_answers ['7'], "RADIO7", key = 'INPUT7', default=False), sg.Radio('-4;2;10', "RADIO7", default = False), sg.Radio('(0;0;0)', "RADIO7", default = False)],
+        [sg.Text(data_fizik_questions ['8'])],
+        [sg.Radio('230', "RADIO8", default = False), sg.Radio('0', "RADIO8", default = False), sg.Radio('400', "RADIO8", default = False), sg.Radio(data_fizik_answers['8'], "RADIO8", key = 'INPUT8', default=False)],
+        [sg.Text(data_fizik_questions ['9'])],
+        [sg.Radio(data_fizik_answers ['9'], "RADIO9", key = 'INPUT9', default=False), sg.Radio('(0;2)', "RADIO9", default = False), sg.Radio('-99;-4', "RADIO9", default = False), sg.Radio('2;∞', "RADIO9", default = False)],
+        [sg.Text(data_fizik_questions ['10'])],
+        [sg.Radio(data_fizik_answers['10'], "RADIO10", key = 'INPUT10', default=False), sg.Radio('240', "RADIO10", default = False), sg.Radio('7', "RADIO10", default = False), sg.Radio('150', "RADIO10", default = False)],
+        [sg.Text(data_fizik_questions ['11'])],
+        [sg.Radio('3a-5', "RADIO11", default = False), sg.Radio('a', "RADIO11", default = False), sg.Radio('-a', "RADIO11", default = False), sg.Radio(data_fizik_answers ['11'], "RADIO11", key = 'INPUT11', default=False)],
+        [sg.Text(data_fizik_questions ['12'])],
+        [sg.Radio('5', "RADIO12", default = False), sg.Radio(data_fizik_answers['12'], "RADIO12", key = 'INPUT12', default=False), sg.Radio('0', "RADIO12", default = False), sg.Radio('144', "RADIO12", default = False)],
+        [sg.Text(data_fizik_questions ['13'])],
+        [sg.Radio('5-3n', "RADIO13", default = False), sg.Radio('2n', "RADIO13", default = False), sg.Radio(data_fizik_answers ['13'], "RADIO13", key = 'INPUT13', default=False), sg.Radio('-n', "RADIO13", default = False)],
+        [sg.Text(data_fizik_questions ['14'])],
+        [sg.Radio(data_fizik_answers['14'], "RADIO14", key = 'INPUT14', default=False), sg.Radio('(4;10)', "RADIO14", default = False), sg.Radio('7;21', "RADIO14", default = False), sg.Radio('-4;-2', "RADIO14", default = False)],
+        [sg.Text(data_fizik_questions ['15'])],
+        [sg.Radio('55см', "RADIO15", default = False), sg.Radio(data_fizik_answers ['15'], "RADIO15", key = 'INPUT15', default=False), sg.Radio('Інше', "RADIO15", default = False), sg.Radio('39см', "RADIO15", default = False)],
+        [sg.Text(data_fizik_questions ['16'])],
+        [sg.Radio('865', "RADIO16", default = False), sg.Radio(data_fizik_answers['16'], "RADIO16", key = 'INPUT16', default=False), sg.Radio('144', "RADIO16", default = False), sg.Radio('920', "RADIO16", default = False)],
+        [sg.Text(data_fizik_questions ['17'])],
+        [sg.Radio('[7; 55]', "RADIO17", default = False), sg.Radio('(-11; 23)', "RADIO17", default = False), sg.Radio(data_fizik_answers ['17'], "RADIO17", key = 'INPUT17', default=False), sg.Radio('(-31; -13)', "RADIO17", default = False)],
+        [sg.Text(data_fizik_questions ['18'])],
+        [sg.Radio(data_fizik_answers['18'], "RADIO18", key = 'INPUT18', default=False), sg.Radio('Чотири', "RADIO18", default = False), sg.Radio('Безліч', "RADIO18", default = False), sg.Radio('Дві', "RADIO18", default = False)],
+        [sg.Text(data_fizik_questions ['19'])],
+        [sg.Radio('65', "RADIO19", default = False), sg.Radio(data_fizik_answers ['19'], "RADIO19", key = 'INPUT19', default=False), sg.Radio('50', "RADIO19", default = False), sg.Radio('-75', "RADIO19", default = False)],
+        [sg.Text(data_fizik_questions ['20'])],
+        [sg.Radio('75', "RADIO20", default = False), sg.Radio('35', "RADIO20", default = False), sg.Radio('20', "RADIO20", default = False), sg.Radio(data_fizik_answers['20'], "RADIO20", key = 'INPUT20', default=False)],
+        [sg.Button('Submit', size = (20, 3))]]
 s = 0
 
-layout = [[ sg.Column(col_test, scrollable=True, size = (1000, 950)) ]]
+layout_quiz_math = [[ sg.Column(col_quiz_math, scrollable = True, size = (1000, 950)) ]]
+layout_quiz_fiz = [[  sg.Column(col_quiz_fiz, scrollable = True, size = (1000, 950))  ]]
 
-window_quiz = sg.Window('Quiz', layout, size = (1000,950))
+window_quiz_math = sg.Window('Quiz', layout_quiz_math, size = (1000,950))
+window_quiz_fiz = sg.Window('Quiz',layout_quiz_fiz, size = (1000, 950))
+
+from nav4 import event
 
 while True:
-        event, values = window_quiz.read()
+        event_quiz, values = window_quiz_math
+        event_quiz, values = window_quiz_fiz
         if event == sg.WIN_CLOSED:
                 break
         if event == 'Submit':
@@ -105,4 +197,4 @@ while True:
                         answers_boolean.append (values['INPUT' + str(i)])
                 s = sum(answers_boolean)
                 print (s)
-window_quiz.close()
+window_quiz_math.close()
